d3bf47ff4d3af8092fc867203d60ec8f7d522085
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2018-05-26 19:08:18 +0200

    remove RvB since the link doesn't work
    might even have shut down? not sure.

--- src/commands/User/servers.js
+++ src/commands/User/servers.js
@@ -20,7 +20,7 @@ module.exports = class extends Command {
 			.setDescription('Click the name of the Discord server you want to join!')
 			.addField('Let\'s Play Network', '• [Cow Chop Community](https://discord.gg/cowchop)\n• [Kinda Funny](https://discord.gg/kindafunny)\n• [Sugar Pine 7 Community](https://discord.gg/HUHExdK)\n• [Game Attack/Screwattack G1](https://discord.gg/F8fncjr)', true) // eslint-disable-line max-len
 			.addField('Rooster Teeth', '• [Rooster Teeth Community](https://https://discord.gg/roosterteeth)\n• [Achievement Hunter Community](https://discord.gg/P8cJ9vC)\n• [RTX 2018](https://discord.gg/0oqF8OqUW3gQDZD2)', true) // eslint-disable-line max-len
-			.addField('Rooster Teeth Shows', '• [/r/RWBY](https://discord.gg/rwby)\n• [Red vs. Blue](https://discord.gg/TkeEJ9D)', true);
+			.addField('Rooster Teeth Shows', '• [/r/RWBY](https://discord.gg/rwby)', true);
 		return msg.send('', { disableEveryone: true, embed: embed });
 	}
 
