5b64fd07fc9dee74e3233b6ed67b04b47a37fd55
Michael Cumbers <me@michaelcumbers.ca>
2019-02-22 12:17:15 -0800

    Adding an example config to ease in testing setup.

---
+++ src/config-default.js
@@ -0,0 +1,22 @@
+exports.config = {
+	/**
+     * Suggested RTByte Developer Client Options
+	 * Make a copy of this file with your relevant information and rename it config.js
+     */
+	controlGuild: '',
+	production: false,
+	prefix: '-',
+	preserveSettings: false,
+	commandEditing: true,
+	consoleEvents: {
+		debug: false,
+		error: true,
+		log: true,
+		verbose: true,
+		warn: true,
+		wtf: true
+	}
+};
+
+// The token for this bot to login with
+exports.token = '';
