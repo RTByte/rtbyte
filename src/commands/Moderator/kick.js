545ac272963c8304256a6fa2eeaeb8c94204b724
JShull97 <justinshull2@gmail.com>
2019-02-23 11:38:20 -0600

    edit to kick.js

--- src/commands/Moderator/kick.js
+++ src/commands/Moderator/kick.js
@@ -6,7 +6,6 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			aliases: ['k'],
-			event: 'kickDM',
 			permissionLevel: 5,
 			requiredPermissions: ['KICK_MEMBERS', 'ADD_REACTIONS', 'USE_EXTERNAL_EMOJIS', 'SEND_MESSAGES', 'EMBED_LINKS'],
 			runIn: ['text'],
