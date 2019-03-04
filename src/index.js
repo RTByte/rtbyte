e6151671420ee3352cbe6e1e7977c31a50282c46
Michael Cumbers <me@michaelcumbers.ca>
2019-03-04 14:58:03 -0800

    Fix broken developer permissions

--- src/index.js
+++ src/index.js
@@ -104,8 +104,8 @@ Client.defaultPermissionLevels
 	.add(6, ({ guild, member }) => guild && member.roles.has(guild.settings.roles.moderator), { fetch: true })
 	.add(7, ({ guild, member }) => guild && member.roles.has(guild.settings.roles.administrator), { fetch: true })
 	.add(8, ({ guild, member }) => guild && member === guild.owner, { fetch: true })
-	.add(9, ({ author, client, guild }) => guild.settings.developersAreSuperUsers && client.options.botOwners.includes(author.id), { break: true })
-	.add(10, ({ author, client, guild }) => guild.settings.developersAreSuperUsers && client.options.botOwners.includes(author.id));
+	.add(9, ({ author, client, guild }) => guild.settings.developmentSettings.developersAreSuperUsers && client.options.botOwners.includes(author.id), { break: true })
+	.add(10, ({ author, client, guild }) => guild.settings.developmentSettings.developersAreSuperUsers && client.options.botOwners.includes(author.id));
 
 class Bot extends Client {}
 
