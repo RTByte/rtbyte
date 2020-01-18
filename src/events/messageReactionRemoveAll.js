ee1a2962534c4bc0ccb85498f5aeb7cd0035adb8
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-01-18 15:10:44 +0100

    feature(starboard): add support for messageReactionRemoveAll event

---
+++ src/events/messageReactionRemoveAll.js
@@ -0,0 +1,27 @@
+const { Event } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, { event: 'messageReactionRemoveAll' });
+	}
+
+	async run(msg) {
+		const starboardChannel = await this.client.channels.get(msg.guild.settings.boards.starboard.starboardChannel);
+		if (!msg.guild.settings.boards.starboard.starboardEnabled) return;
+
+		const starred = msg.guild.settings.boards.starboard.starred.find(star => star.msgID === msg.id);
+
+		if (starred) {
+			await starboardChannel.messages.fetch(starred.starID)
+				.then(message => {
+					msg.guild.settings.update('boards.starboard.starred', starred, { action: 'remove' });
+					message.delete();
+				});
+		}
+
+		return;
+	}
+
+};
