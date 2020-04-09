07daab820215bc97d47fff8d46c2aa803fe95a73
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-04-09 20:47:01 +0200

    add additional information to inviteDelete from audit log

--- src/events/inviteDelete.js
+++ src/events/inviteDelete.js
@@ -1,6 +1,5 @@
 const { Event } = require('klasa');
 const { MessageEmbed } = require('discord.js');
-const moment = require('moment-timezone');
 
 module.exports = class extends Event {
 
@@ -9,14 +8,17 @@ module.exports = class extends Event {
 	}
 
 	async run(invite) {
-		if (invite.guild.available && invite.guild.settings.get('channels.log') && invite.guild.settings.get('logs.events.inviteDelete')) await this.inviteDeleteLog(invite);
+		const auditLog = await invite.guild.fetchAuditLogs();
+		const entry = await auditLog.entries.first();
+
+		if (invite.guild.available && invite.guild.settings.get('channels.log') && invite.guild.settings.get('logs.events.inviteDelete')) await this.inviteDeleteLog(invite, entry);
 
 		return;
 	}
 
-	async inviteDeleteLog(invite) {
-		const affirmEmoji = this.client.emojis.get(this.client.settings.get('emoji.affirm'));
-
+	async inviteDeleteLog(invite, entry) {
+		const { executor } = entry;
+		const uses = entry.changes[3].old;
 		const embed = new MessageEmbed()
 			.setAuthor(`discord.gg/${invite.code}`, invite.guild.iconURL())
 			.setDescription(invite.url)
@@ -24,7 +26,9 @@ module.exports = class extends Event {
 			.addField(invite.channel.type === 'text' ? invite.guild.language.get('CHANNEL') : invite.guild.language.get('VOICE_CHANNEL'),
 				invite.channel.type === 'text' ? invite.channel : `${invite.channel.name}`, true)
 			.setTimestamp()
-			.setFooter(invite.guild.language.get('GUILD_LOG_INVITEDELETE'));
+			.setFooter(invite.guild.language.get('GUILD_LOG_INVITEDELETE', executor), executor.displayAvatarURL());
+
+		if (uses > 0) await embed.addField(invite.guild.language.get('GUILD_LOG_INVITEDELETE_USES'), uses, true);
 
 		const logChannel = await this.client.channels.get(invite.guild.settings.get('channels.log'));
 		await logChannel.send('', { disableEveryone: true, embed: embed });
