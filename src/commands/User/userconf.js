a3bccaa9de8fdf9268f8231a88372f851ad84ce8
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-04-11 00:01:13 +0200

    disabling userconf as it's never used

--- src/commands/User/userconf.js
+++ src/commands/User/userconf.js
@@ -4,6 +4,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
+			enabled: false,
 			guarded: true,
 			subcommands: true,
 			description: language => language.get('COMMAND_CONF_USER_DESCRIPTION'),
