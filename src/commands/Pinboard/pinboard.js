f30c80d703ab35a7f6fd89fc634b07b08161145a
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-04-12 18:41:07 +0200

    pinboard command series to let mods manage pinboard settings

--- src/commands/Pinboard/pinboard.js
+++ src/commands/Pinboard/pinboard.js
@@ -9,13 +9,8 @@ module.exports = class extends Command {
 			description: language => language.get('COMMAND_PINBOARD_DESCRIPTION'),
 			runIn: ['text'],
 			subcommands: true,
-			usage: '<enable|disable|set|remove|reset|show:default> (channel|ignored) [specifiedChannel:channel]',
+			usage: '<enable|disable|set|remove|reset|show:default> [channel|ignored] [specifiedChannel:channel]',
 			usageDelim: ' '
-		});
-		this.createCustomResolver('channel', (arg, possible, message, [action]) => {
-			if (!['set', 'reset'].includes(action)) return null;
-			if (arg) return this.client.arguments.get('string').run(arg, possible, message);
-			throw message.language.get('COMMAND_CONF_NOVALUE');
 		});
 	}
 
@@ -32,11 +27,11 @@ module.exports = class extends Command {
 		const pinboardIgnoredChannels = msg.guild.settings.get('boards.pinboard.pinboardIgnoredChannels').map(channel => msg.guild.channels.get(channel)).join(', ') || 'None';
 
 		const embed = new MessageEmbed()
-			.setAuthor('Pinboard settings', this.client.user.displayAvatarURL())
+			.setAuthor(msg.language.get('COMMAND_PINBOARD_SHOW_TITLE'), this.client.user.displayAvatarURL())
 			.setColor(this.client.settings.get('colors.white'))
-			.addField('On', pinboardEnabled, true)
-			.addField('Channel', pinboardChannel, true)
-			.addField('Ignored channels', pinboardIgnoredChannels)
+			.addField(msg.language.get('COMMAND_PINBOARD_SHOW_ENABLED'), pinboardEnabled, true)
+			.addField(msg.language.get('CHANNEL'), pinboardChannel, true)
+			.addField(msg.language.get('COMMAND_PINBOARD_SHOW_IGNORED'), pinboardIgnoredChannels)
 			.setThumbnail(msg.guild.iconURL(), 50, 50)
 			.setTimestamp()
 			.setFooter(msg.language.get('COMMAND_REQUESTED_BY', msg), msg.author.displayAvatarURL());
@@ -47,24 +42,65 @@ module.exports = class extends Command {
 	async enable(msg) {
 		const pinboardEnabled = msg.guild.settings.get('boards.pinboard.pinboardEnabled');
 
-		if (pinboardEnabled) return msg.reject('The pinboard is already enabled.');
- 
+		if (pinboardEnabled) return msg.reject(msg.language.get('COMMAND_PINBOARD_ENABLE_ALREADYENABLED'));
+
 		await msg.guild.settings.update('boards.pinboard.pinboardEnabled', true);
-		return msg.affirm('Enabled the pinboard.');
+		return msg.affirm(msg.language.get('COMMAND_PINBOARD_ENABLE_SUCCESS'));
 	}
 
 	async disable(msg) {
 		const pinboardEnabled = msg.guild.settings.get('boards.pinboard.pinboardEnabled');
 
-		if (!pinboardEnabled) return msg.reject('The pinboard is already disabled.');
+		if (!pinboardEnabled) return msg.reject(msg.language.get('COMMAND_PINBOARD_DISABLE_ALREADYDISABLED'));
 
 		await msg.guild.settings.update('boards.pinboard.pinboardEnabled', false);
-		return msg.affirm('Disabled the pinboard.');
+		return msg.affirm(msg.language.get('COMMAND_PINBOARD_DISABLE_SUCCESS'));
 	}
 
-	async set(msg, [type, channel]) {
-		console.log('working');
-		console.log(type, channel.name);
+	async set(msg, [setting, specifiedChannel]) {
+		const pinboardChannel = msg.guild.settings.get('boards.pinboard.pinboardChannel');
+		const pinboardIgnoredChannels = msg.guild.settings.get('boards.pinboard.pinboardIgnoredChannels');
+
+		if (setting === 'channel') setting = 'pinboardChannel';
+		if (setting === 'ignored') setting = 'pinboardIgnoredChannels';
+
+		if (specifiedChannel.id === pinboardChannel) return msg.reject(msg.language.get('COMMAND_PINBOARD_SET_CHANNEL_SAMECHANNEL', specifiedChannel));
+		if (pinboardIgnoredChannels.includes(specifiedChannel.id)) return msg.reject(msg.language.get('COMMAND_PINBOARD_SET_IGNORED_ALREADYADDED', specifiedChannel));
+
+		await msg.guild.settings.update(`boards.pinboard.${setting}`, specifiedChannel);
+		if (setting === 'pinboardChannel') return msg.affirm(msg.language.get('COMMAND_PINBOARD_SET_CHANNEL_SUCCESS', specifiedChannel));
+		if (setting === 'pinboardIgnoredChannels') return msg.affirm(msg.language.get('COMMAND_PINBOARD_SET_IGNORED_SUCCESS', specifiedChannel));
+
+		return true;
+	}
+
+	async remove(msg, [setting, specifiedChannel]) {
+		const pinboardIgnoredChannels = msg.guild.settings.get('boards.pinboard.pinboardIgnoredChannels');
+
+		if (setting !== 'ignored') return msg.reject(msg.language.get('COMMAND_PINBOARD_REMOVE_ONLYIGNORED'));
+		if (!pinboardIgnoredChannels.find(channel => channel === specifiedChannel.id)) {
+			return msg.reject(msg.language.get('COMMAND_PINBOARD_REMOVE_NOTADDED', specifiedChannel));
+		}
+
+		await msg.guild.settings.update('boards.pinboard.pinboardIgnoredChannels', specifiedChannel, { action: 'remove' });
+		return msg.affirm(msg.language.get('COMMAND_PINBOARD_REMOVE_SUCCESS', specifiedChannel));
+	}
+
+	async reset(msg, [setting]) {
+		const pinboardChannel = msg.guild.settings.get('boards.pinboard.pinboardChannel');
+		const pinboardIgnoredChannels = msg.guild.settings.get('boards.pinboard.pinboardIgnoredChannels');
+
+		if (setting === 'channel') setting = 'pinboardChannel';
+		if (setting === 'ignored') setting = 'pinboardIgnoredChannels';
+
+		if (setting === 'pinboardChannel' && !pinboardChannel) return msg.reject(msg.language.get('COMMAND_PINBOARD_RESET_CHANNEL_NOTSET'));
+		if (setting === 'pinboardIgnoredChannels' && !pinboardIgnoredChannels.length) return msg.reject(msg.language.get('COMMAND_PINBOARD_RESET_IGNORED_NOTSET'));
+
+		await msg.guild.settings.reset(`boards.pinboard.${setting}`);
+		if (setting === 'pinboardChannel') return msg.affirm(msg.language.get('COMMAND_PINBOARD_RESET_CHANNEL_SUCCESS'));
+		if (setting === 'pinboardIgnoredChannels') return msg.affirm(msg.language.get('COMMAND_PINBOARD_RESET_IGNORED_SUCCESS'));
+
+		return true;
 	}
 
 };
