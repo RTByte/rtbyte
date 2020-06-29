ffb6ad48a2b9d7627f13ac8764464d100a8df8de
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-06-29 19:22:09 +0200

    Fix double unban logs when banning via command
    Also doesn't submit an unban case when a softban is done

--- src/events/rtbyteGuildBanRemove.js
+++ src/events/rtbyteGuildBanRemove.js
@@ -10,20 +10,19 @@ module.exports = class extends Event {
 	async run(guild, user) {
 		if (!guild) return;
 
-		let executor, bans, banInfo;
+		let executor, banInfo;
 		if (guild.me.hasPermission('VIEW_AUDIT_LOG')) {
 			const auditLog = await guild.fetchAuditLogs();
 			const logEntry = await auditLog.entries.first();
 
-			if (logEntry.action === 'MEMBER_BAN_REMOVE') executor = logEntry ? logEntry.executor : undefined;
-		}
-		if (guild.me.hasPermission('BAN_MEMBERS')) {
-			bans = await guild.fetchBans();
-			banInfo = await bans.get(user.id);
+			if (logEntry.action === 'MEMBER_BAN_REMOVE') {
+				executor = logEntry ? logEntry.executor : undefined;
+				banInfo = logEntry ? logEntry.reason : undefined;
+			}
 		}
 
-		// Ignore if ban was initiated by command
-		if (banInfo && banInfo.reason ? banInfo.reason.endsWith('(fc)') : false) return;
+		// Ignore if ban was initiated by command or is a softban being released.
+		if (banInfo ? banInfo.endsWith('(fc)') || banInfo === 'Softban released.' : false) return;
 
 		const modCase = new ModCase(guild)
 			.setType('unban')
