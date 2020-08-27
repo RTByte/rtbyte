6e2e40fb275d0d44a7123618abdb49f980b32f0b
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-08-27 23:57:07 +0200

    remove internal error logging as it's now handled through sentry

--- src/events/rtbyteEventError.js
+++ src/events/rtbyteEventError.js
@@ -1,7 +1,5 @@
 const { Event } = require('klasa');
-const { MessageEmbed } = require('discord.js');
 const Sentry = require('@sentry/node');
-const errorEvents = ['rtbyteCommandError', 'rtbyteEventError', 'rtbyteFinalizerError', 'rtbyteMonitorError', 'rtbyteTaskError'];
 
 module.exports = class extends Event {
 
@@ -16,26 +14,6 @@ module.exports = class extends Event {
 
 			Sentry.captureException(error);
 		});
-
-		const guild = args[0].guild ? args[0].guild : args[0].message ? args[0].message.guild : args[0];
-		if (this.client.settings.get('logs.eventError') && !errorEvents.includes(event.name)) await this.eventErrorLog(event, args, error, guild);
-	}
-
-	async eventErrorLog(event, args, error, guild) {
-		const globalLog = await this.client.channels.cache.get(this.client.settings.get('channels.globalLog'));
-
-		const embed = new MessageEmbed()
-			.setAuthor(this.client.user.username, this.client.user.displayAvatarURL())
-			.setColor(this.client.settings.get('colors.red'))
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_EXECUTEDIN'), `\`${guild.name} (${guild.id})\``)
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_ORIGIN'), `\`\`\`${event.path}\`\`\``)
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_ERRORSTACK'), `\`\`\`${error ? error.stack ? error.stack : error : 'Unknown error'}\`\`\``)
-			.setTimestamp()
-			.setFooter(globalLog.guild.language.get('GLOBAL_LOG_EVENTERROR'));
-
-		if (globalLog) await globalLog.send('', { disableEveryone: true, embed: embed });
-
-		return;
 	}
 
 };
--- src/events/rtbyteMonitorError.js
+++ src/events/rtbyteMonitorError.js
@@ -1,5 +1,4 @@
 const { Event } = require('klasa');
-const { MessageEmbed } = require('discord.js');
 const Sentry = require('@sentry/node');
 
 module.exports = class extends Event {
@@ -16,25 +15,6 @@ module.exports = class extends Event {
 
 			Sentry.captureException(error);
 		});
-
-		if (this.client.settings.get('logs.monitorError')) await this.monitorErrorLog(message, monitor, error);
-	}
-
-	async monitorErrorLog(message, monitor, error) {
-		const globalLog = await this.client.channels.cache.get(this.client.settings.get('channels.globalLog'));
-
-		const embed = new MessageEmbed()
-			.setAuthor(this.client.user.username, this.client.user.displayAvatarURL())
-			.setColor(this.client.settings.get('colors.red'))
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_EXECUTEDIN'), message.guild ? `\`${message.guild.name} (${message.guild.id})\`` : `\`${message.author.tag} (${message.author.id})\``)
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_ORIGIN'), `\`\`\`${monitor.path}\`\`\``)
-			.addField(globalLog.guild.language.get('GLOBAL_LOG_ERROR_ERRORSTACK'), `\`\`\`${error ? error.stack ? error.stack : error : 'Unknown error'}\`\`\``)
-			.setTimestamp()
-			.setFooter(globalLog.guild.language.get('GLOBAL_LOG_MONITORERROR'));
-
-		if (globalLog) await globalLog.send('', { disableEveryone: true, embed: embed });
-
-		return;
 	}
 
 };
