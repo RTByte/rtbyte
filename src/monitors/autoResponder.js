9830ab12e17901b0ae7ae51df978622981623a86
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2020-08-28 12:16:17 +0200

    fix: support for autoresponder keyphrase or word containing symbols

--- src/monitors/autoResponder.js
+++ src/monitors/autoResponder.js
@@ -16,10 +16,12 @@ module.exports = class extends Monitor {
 		if (!msg.guild.settings.get('autoResponder.autoResponderEnabled') || !msg.guild.settings.get('autoResponder.autoResponses').length) return;
 		if (msg.command) return;
 
-		const msgContent = msg.content.toLowerCase();
 		for (const response of msg.guild.settings.get('autoResponder.autoResponses')) {
-			const keyword = new RegExp(`\\b(${response.name})\\b`, 'g');
-			if (msgContent.match(keyword)) msg.send(response.content);
+			const symbolChecker = new RegExp('[^A-Za-z0-9]', 'g');
+			const keyword = response.name.match(symbolChecker) ? new RegExp(`\\b(${response.name})\\B`, 'gi') : new RegExp(`\\b(${response.name})\\b`, 'gi');
+			const message = response.content.replace('{user}', `${msg.author}`);
+
+			if (msg.content.match(keyword)) msg.send(message);
 		}
 	}
 
