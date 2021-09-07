efb15196565261c94cb815a2263cb2ca401eb425
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2021-09-07 22:02:52 +0200

    ts-rewrite 2: electric boogaloo - more cleanup

--- src/lib/types/Augments.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { FirestoreManager } from '@lib/structures/FirestoreManager';
-
-declare module 'discord.js' {
-	interface Client {
-		readonly firestore: FirestoreManager;
-	}
-}
--- src/types/Augments.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import type { FirestoreManager } from '@lib/structures/FirestoreManager';
-
-declare module 'discord.js' {
-	interface Client {
-		readonly firestore: FirestoreManager;
-	}
-}
