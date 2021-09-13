8a7c3ea2ba0c15e03aabbac36aeb63096041a078
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2021-09-13 18:36:43 +0200

    drop timezone from prisma schema as it will go unused

---
+++ prisma/migrations/20210913163155_remove_timezone_config/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `timezone` on the `guild` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "guild" DROP COLUMN "timezone";
