37ff2312a7083c62142405847bb799b6986ce419
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2021-09-13 18:37:49 +0200

    refactor(commands/enlarge): use sendTemporaryMsg for invalid input

--- src/commands/User/enlarge.ts
+++ src/commands/User/enlarge.ts
@@ -1,6 +1,7 @@
 import { LanguageKeys } from '#lib/i18n/languageKeys';
 import { RTByteCommand } from '#lib/structures';
 import { REGEX } from '#utils/constants';
+import { sendTemporaryMessage } from '#utils/functions';
 import { ApplyOptions } from '@sapphire/decorators';
 import { reply } from '@sapphire/plugin-editable-commands';
 import { Message, MessageAttachment, Permissions } from 'discord.js';
@@ -16,7 +17,7 @@ export class UserCommand extends RTByteCommand {
 	public async run(message: Message, args: RTByteCommand.Args) {
 		const input = await args.pickResult('string');
 
-		if (!input.success) return reply(message, args.t(LanguageKeys.Commands.User.EnlargeNoneSpecified))
+		if (!input.success) return sendTemporaryMessage(message, args.t(LanguageKeys.Commands.User.EnlargeNoneSpecified))
 
 		// Parse native emoji
 		const emoji = parseEmoji(input.value, { assetType: 'png' })[0];
@@ -27,11 +28,11 @@ export class UserCommand extends RTByteCommand {
 		const customEmoji = customEmojiRegex ? customEmojiRegex[1] : null;
 
 		// Return if neither emoji type can be parsed
-		if (!emoji && !customEmoji) return reply(message, args.t(LanguageKeys.Commands.User.EnlargeInvalidInput, { input: input.value }))
+		if (!emoji && !customEmoji) return sendTemporaryMessage(message, args.t(LanguageKeys.Commands.User.EnlargeInvalidInput, { input: input.value }))
 
 		const emojiAttachment = new MessageAttachment(emoji
 			? emoji.url
 			: `https://cdn.discordapp.com/emojis/${customEmoji}.${animated ? 'gif' : 'png'}`);
-		return reply(message, {files: [emojiAttachment] });
+		return reply(message, { files: [emojiAttachment] });
 	}
 }
