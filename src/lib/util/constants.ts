6fbd0e3c8ab19da936044d8e6e5ce6be132c8b94
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2021-09-15 11:54:54 +0200

    refactor(constants): use emojis from prod guild

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -17,21 +17,21 @@ export const enum Colors {
 }
 
 export const enum Emojis {
+	ArrowLeft = '<:rtbyte_arrow_left:801917830710558790>',
+	ArrowRight = '<:rtbyte_arrow_right:801918001124212837>',
+	ArrowToLeft = '<:rtbyte_arrow_to_left:801918098571395133>',
+	ArrowToRight = '<:rtbyte_arrow_to_right:801918207077777409>',
+	BotBadge = '<:rtbyte_bot_badge:801919189664399391>',
 	Check = '<:check:801916142338834492>',
+	Dnd = '<:rtbyte_dnd:801918926417559552>',
+	Idle = '<:rtbyte_idle:801918753679212544>',
+	Info = '<:rtbyte_info:801918407023919124>',
+	List = '<:rtbyte_list:801918507666374679>',
+	Offline = '<:rtbyte_offline:801919060303806504>',
+	Online = '<:rtbyte_online:801918663233241099>',
+	PartnerBadge = '<:rtbyte_partnered_badge:801919416429838366>',
+	VerifiedBadge = '<:rtbyte_verified_badge:801919582696505364>',
 	X = '<:cross:801916142327169114>',
-	ArrowLeft = '',
-	ArrowRight = '',
-	ArrowToLeft = '',
-	ArrowToRight = '',
-	Info = '',
-	List = '',
-	Online = '',
-	Idle = '',
-	Dnd = '',
-	Offline = '',
-	BotBadge = '',
-	PartnerBadge = '',
-	VerifiedBadge = ''
 }
 
 export const enum LanguageFormatters {
