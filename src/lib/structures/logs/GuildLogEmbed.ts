bcfff07c73c6e72f7df63c0c590a4b3c10816c7f
Rasmus Gerdin <rasmus@rasmusgerdin.com>
2021-09-28 02:01:03 +0200

    feat(listeners): add stage instance logs

--- src/lib/structures/logs/GuildLogEmbed.ts
+++ src/lib/structures/logs/GuildLogEmbed.ts
@@ -13,31 +13,34 @@ export class GuildLogEmbed extends MessageEmbed {
 			case Events.ChannelCreate:
 			case Events.EmojiCreate:
 			case Events.GuildMemberAdd:
+			case Events.GuildWebhookCreate:
 			case Events.InviteCreate:
 			case Events.RoleCreate:
+			case Events.StageInstanceCreate:
 			case Events.StickerCreate:
 			case Events.ThreadCreate:
-			case Events.GuildWebhookCreate:
 				this.setColor(Colors.Green);
 				break;
 			case Events.ChannelDelete:
 			case Events.EmojiDelete:
 			case Events.GuildMemberRemove:
+			case Events.GuildWebhookDelete:
 			case Events.InviteDelete:
 			case Events.RoleDelete:
+			case Events.StageInstanceDelete:
 			case Events.StickerDelete:
 			case Events.ThreadDelete:
-			case Events.GuildWebhookDelete:
 				this.setColor(Colors.Red);
 				break;
 			case Events.ChannelUpdate:
 			case Events.EmojiUpdate:
 			case Events.GuildMemberUpdate:
+			case Events.GuildWebhookUpdate:
 			case Events.GuildUpdate:
 			case Events.RoleUpdate:
+			case Events.StageInstanceUpdate:
 			case Events.StickerUpdate:
 			case Events.ThreadUpdate:
-			case Events.GuildWebhookUpdate:
 				this.setColor(Colors.Blue);
 				break;
 			case Events.MessageDelete:
